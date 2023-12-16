ea1640b7c029c8cdae68b3d9fac31886054406f4
Greg Perkins <greg.perkins@cradlepoint.com>
2023-12-15 23:50:19 -0700

    day 16

---
+++ day16/part2.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+from aoc_util import *
+set_cases(default_idx=0, cases=[('example0.txt', 51), ('input.txt', 8437)])
+
+grid = [line for line in case_lines()]
+height = len(grid)
+width = len(grid[0])
+
+def step(loc, dir):
+    x,y = loc
+    dx,dy = dir
+    return (x+dx,y+dy)
+
+def in_bounds(loc):
+    x,y = loc
+    return x>=0 and x<width and y>=0 and y<height
+
+optics = {
+    ('.', (0,-1)): [(0,-1)],
+    ('.', (+1,0)): [(+1,0)],
+    ('.', (0,+1)): [(0,+1)],
+    ('.', (-1,0)): [(-1,0)],
+
+    ('/', (0,-1)): [(+1,0)],
+    ('/', (+1,0)): [(0,-1)],
+    ('/', (0,+1)): [(-1,0)],
+    ('/', (-1,0)): [(0,+1)],
+
+    ('\\',(0,-1)): [(-1,0)],
+    ('\\',(+1,0)): [(0,+1)],
+    ('\\',(0,+1)): [(+1,0)],
+    ('\\',(-1,0)): [(0,-1)],
+
+    ('-', (0,-1)): [(-1,0), (+1,0)],
+    ('-', (+1,0)): [(+1,0)],
+    ('-', (0,+1)): [(-1,0), (+1,0)],
+    ('-', (-1,0)): [(-1,0)],
+ 
+    ('|', (0,-1)): [(0,-1)],
+    ('|', (+1,0)): [(0,-1), (0,+1)],
+    ('|', (0,+1)): [(0,+1)],
+    ('|', (-1,0)): [(0,-1), (0,+1)],
+}
+
+def trace(first_loc, first_dir):
+    notes = defaultdict(lambda: set())
+
+    queue = deque([(first_loc, first_dir)])
+    while queue:
+        loc,dir = queue.popleft()
+        x,y = loc
+
+        if dir not in notes[loc]:
+            notes[loc].add(dir)
+            for next_dir in optics[(grid[y][x], dir)]:
+                next_loc = step(loc, next_dir)
+                if in_bounds(next_loc):
+                    queue.append((next_loc, next_dir))
+    
+    return sum(1 if notes[(x,y)] else 0 for x in range(width) for y in range(height))
+
+
+top    = [((x,0),        (0,+1)) for x in range(width) ]
+bottom = [((x,height-1), (0,-1)) for x in range(width) ]
+left   = [((0,y),        (+1,0)) for y in range(height)]
+right  = [((width-1,y),  (-1,0)) for y in range(height)]
+
+assert_solution(max(trace(loc, dir) for loc, dir in top + bottom + left + right))
